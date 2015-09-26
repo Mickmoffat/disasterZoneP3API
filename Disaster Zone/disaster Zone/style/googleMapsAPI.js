1993c9d7ed8ee313c18ea3e579fce106b6c3525b
Mickmoffat <mickmoffat@gmail.com>
2015-09-26 13:28:45 +1200

    9.6 - CUSTOM UI ELEMENTS
    Creating custom UI elements for google api to replace current HTML + CSS
    key div.

--- Disaster Zone/disaster Zone/style/googleMapsAPI.js
+++ Disaster Zone/disaster Zone/style/googleMapsAPI.js
@@ -1,6 +1,39 @@
 /**** GOOGLE MAPS API Disaster Zone MDDN201 P3 [2015] (300317288) ****/
 
 /*** CUSTOM SETTINGS ***/
+var map;
+
+/** CUSTOM UI ELEMENTS  START **/
+function CenterControl(controlDiv, map) {
+
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click to recenter the map';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Center Map';
+    controlUI.appendChild(controlText);
+
+    
+
+}
+
+/*** INITAL MAP ***/
 
 function initMap() {
     /****** DISASTER ZONE VARABLES POS ******/
@@ -20,11 +53,37 @@ function initMap() {
     /*HURRICANE BRAZIL*/
     var brazil = new google.maps.LatLng(-15.4700, -47.5500); //pos
 
-
+    /* SET MAP OPTIONS */
     var map = new google.maps.Map(document.getElementById('googleAPI'), {
-        zoom: 4,
-        center: { lat: -25.363882, lng: 131.044922 } //starting locaiton of google maps
+
+        //MAP OPTIONS
+        zoom: 4, //sets zoom level
+        draggable: true, //disable drag
+        zoomControl: true, //disable or enable zoom
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        },
+        disableDoubleClickZoom: true, //disables zoom
+        scrollwheel: false, //disables scroll wheel
+        disableDefaultUI: true, //disables UI
+        mapTypeId: google.maps.MapTypeId.TERRAIN, //sets terrain view
+        
+        center: { lat: -25.363882, lng: 131.044922 } //starting NZ
+
+
     });
+
+    /*++ CUSTOM UI START ++*/
+  // Create the DIV to hold the control and call the CenterControl() constructor
+  // passing in this DIV.
+  var centerControlDiv = document.createElement('div');
+  var centerControl = new CenterControl(centerControlDiv, map);
+
+  centerControlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+    /*++ CUSTOM UI END ++*/
+
+
 
 
     /**+CUSTOM ICONS SVG +**/
@@ -63,5 +122,7 @@ function initMap() {
         map: map
     });
 }
+
+
     
 
