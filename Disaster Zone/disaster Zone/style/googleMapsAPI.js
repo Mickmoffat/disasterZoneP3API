6835e986581ac974114f148f771cb3edefbd31d3
Mickmoffat <mickmoffat@gmail.com>
2015-09-27 13:07:48 +1300

    9.7 - hoverStart
    Starting on hover funcitons.

--- Disaster Zone/disaster Zone/style/googleMapsAPI.js
+++ Disaster Zone/disaster Zone/style/googleMapsAPI.js
@@ -126,15 +126,44 @@ function initMap() {
 
 
     /*++++_ DISASTER WEIGHPOINTS _++++*/
+    /** TEXT CONTENT **/
+  var taumarunuiContentText = '<div id ="content">' + '<div id="siteNotice">' +
+      '</div>' +
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+      '<div id="bodyContent">' +
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the ' +
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+      'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+      'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+      'Aboriginal people of the area. It has many springs, waterholes, ' +
+      'rock caves and ancient paintings. Uluru is listed as a World ' +
+      'Heritage Site.</p>' +
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+      '(last visited June 22, 2009).</p>' +
+      '</div>' +
+      '</div>';
 
+  var infowindow = new google.maps.InfoWindow({
+      content: taumarunuiContentText
+  });
+
+  var toolTip = "test";
     /*++++ TAUMARUNI ++++*/
     var marker = new google.maps.Marker({
         position: taumarunui, //position of weighpoint
         icon: './media/img/mapKeys/event/light/earthquakeL.png',
-        animation:google.maps.Animation.BOUNCE,
+        //animation:google.maps.Animation.BOUNCE,
+        title: 'Uluru (Ayers Rock)',
         map: map
     });
-    marker.addListener('click', toggleBounce);
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+    //marker.addListener('click', toggleBounce);
 
     /*+++ PERTH +++*/
     var marker = new google.maps.Marker({
