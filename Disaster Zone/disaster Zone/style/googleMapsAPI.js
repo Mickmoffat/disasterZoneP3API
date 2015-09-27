693389c04354f0f274516bccf9c8a4ff60d56648
Mickmoffat <mickmoffat@gmail.com>
2015-09-27 13:29:24 +1300

    9.7 - InfoWindowsWorking
    Info on click working

--- Disaster Zone/disaster Zone/style/googleMapsAPI.js
+++ Disaster Zone/disaster Zone/style/googleMapsAPI.js
@@ -127,41 +127,55 @@ function initMap() {
 
     /*++++_ DISASTER WEIGHPOINTS _++++*/
     /** TEXT CONTENT **/
-  var taumarunuiContentText = '<div id ="content">' + '<div id="siteNotice">' +
-      '</div>' +
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      '<div id="bodyContent">' +
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the ' +
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-      'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-      'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-      'Aboriginal people of the area. It has many springs, waterholes, ' +
-      'rock caves and ancient paintings. Uluru is listed as a World ' +
-      'Heritage Site.</p>' +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-      '(last visited June 22, 2009).</p>' +
-      '</div>' +
-      '</div>';
 
-  var infowindow = new google.maps.InfoWindow({
-      content: taumarunuiContentText
+  /*_TAUMARUNI EARTHQUAKE_*/
+  var taumarunuiContentText = '<div id ="content">' + '<div id="siteNotice">' +
+    '</div>' +
+    '<h1 id="" class="">Taumarunui Earthquake</h1>' +
+
+    '</div>';
+
+    /**_ PERTH _**/
+
+  var perthContentText = '<div id ="content">' + '<div id="siteNotice">' +
+    '</div>' +
+    '<h1 id="" class="">Perth Bush Fire</h1>' + '<p>test</p>' +
+    
+    '</div>';
+
+    /***_ CHIANG RAI _***/
+
+    /****_ ILLIONIS _****/
+
+    /*****_ BRAZIL _*****/  
+
+    /*__ VARABLES __*/
+
+    /*_TAUMARUNI EARTHQUAKE_*/
+  var taumarunuiInfoWindow = new google.maps.InfoWindow({
+      content: taumarunuiContentText,
   });
 
-  var toolTip = "test";
+    /**_ PERTH _**/
+  var perthInfoWindow = new google.maps.InfoWindow({
+      content: perthContentText,
+  });
+
+  
     /*++++ TAUMARUNI ++++*/
     var marker = new google.maps.Marker({
         position: taumarunui, //position of weighpoint
         icon: './media/img/mapKeys/event/light/earthquakeL.png',
         //animation:google.maps.Animation.BOUNCE,
-        title: 'Uluru (Ayers Rock)',
+        title: 'Taumarunui', //display on hover
+        infowindow: taumarunuiInfoWindow, //info window variable
         map: map
     });
-    marker.addListener('click', function () {
-        infowindow.open(map, marker);
+
+    /* CLICK DISPLAY INFOWINDOW */
+    google.maps.event.addListener(marker, 'click', function () {
+        this.infowindow.open(map, this);
+
     });
     //marker.addListener('click', toggleBounce);
 
@@ -169,9 +183,16 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: perth, //position of weighpoint
         icon: './media/img/mapKeys/event/severe/fireS.png',
+        title: 'Perth', //display on hover
+        infowindow: perthInfoWindow, //info window variable
         map: map
     });
 
+    /* CLICK DISPLAY INFOWINDOW */
+    google.maps.event.addListener(marker, 'click', function () {
+        this.infowindow.open(map, this);
+
+    });
     /*++ CHIANG RAI ++*/
     var marker = new google.maps.Marker({
         position: chiangRai, //position of weighpoint
